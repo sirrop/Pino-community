ee33a4bee7713743f5ff71765d43f101270f5e35
branc <shinta0035@gmail.com>
2021-08-13 11:49:50 +0900

    設定ダイアログのスタイルを調整

--- src/main/java/jp/gr/java_conf/alpius/pino/ui/actionSystem/scripts/show_env_config.groovy
+++ src/main/java/jp/gr/java_conf/alpius/pino/ui/actionSystem/scripts/show_env_config.groovy
@@ -7,11 +7,15 @@ import javafx.scene.Node
 import javafx.scene.control.ButtonType
 import javafx.scene.control.Dialog
 
+import java.nio.file.Paths
+
 def loader = new FXMLLoader()
 def parent = loader.load EnvConfigDialogController.class.getResourceAsStream("EnvConfigDialog.fxml")
 EnvConfigDialogController controller = loader.controller
 
 def dialog = new Dialog()
+dialog.dialogPane.styleClass.add("config-dialog")
+dialog.dialogPane.stylesheets.add(Paths.get("data", "pino-white.css").toUri().toURL().toExternalForm())
 dialog.dialogPane.buttonTypes.addAll ButtonType.APPLY, ButtonType.CANCEL, ButtonType.OK
 dialog.dialogPane.content = parent as Node
 dialog.dialogPane.lookupButton(ButtonType.APPLY).addEventFilter(ActionEvent.ACTION) {
